0c4322c0e54adaf3624512326ed0232a1fdd8504
Ashish <ashish10211@gmail.com>
2019-07-28 23:10:01 +1000

    Update devices.js

--- src/devices.js
+++ src/devices.js
@@ -18,10 +18,6 @@ import {Circle} from 'react-shapes';
   }
 
   componentWillMount(){
-    console.log(this.state.name)
-     console.log(this.state.email)
-      console.log(this.state.message)
-       console.log(this.state.repoURL)
     this.fetchdata();
      this.fetchToken();
      this.interval = setInterval(() => {
